7052b7b7a362af7a577d6089444b0f4cb8d06f91
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-03 18:33:51 +0100

    Init + player movement and rotation

---
+++ Prototype/Assets/Scripts/Command/CommandManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandManager : MonoBehaviour
+{
+    // Queue that contains all the commands that come from the server
+    static Queue<Command> commandQueue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        commandQueue = new Queue<Command>();
+    }
+
+    // Process as many commands as you can in the Fixed Timestep of the FixedUpdate
+    void FixedUpdate()
+    {
+        float startTime = Time.deltaTime;
+
+        while(Time.deltaTime - startTime < Time.fixedDeltaTime)
+        {
+            ProcessCommand();
+        }
+    }
+
+    // Add a command to the queue, this sould be called from the outside
+    public void AddCommand(Command command)
+    {
+        commandQueue.Enqueue(command);
+    }
+
+    // 
+    void ProcessCommand()
+    {
+        Command command = commandQueue.Dequeue();
+        command.execute();
+    }
+}
