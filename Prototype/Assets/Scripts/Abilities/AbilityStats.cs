81190df5deab0e32cd59b21cbea5189060799435
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-05 18:03:07 +0100

    Started abilities

--- Prototype/Assets/Scripts/Abilities/AbilityStats.cs
+++ Prototype/Assets/Scripts/Abilities/AbilityStats.cs
@@ -1,17 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public struct AbilityStats
 {
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public int cooldown;      // Time till you can cast the ability again
+    public int manaCost;      // How much mana will this ability cost?
+    public int dotDamage;     // Damage over time damage tick
+    public int dotDuration;   // Damage over time durration
+    public int hpDiff;        // If positive it's a heal if it's negative it's damage 
 }
