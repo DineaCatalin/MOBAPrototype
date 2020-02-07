9e80ce1fda2e87cb8642a0688e361691faf514a8
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-07 19:17:01 +0100

    Äbility rework

---
+++ Prototype/Assets/Scripts/Utils/LookAtMouse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtMouse : MonoBehaviour
+{
+    Vector3 direction;
+    float angle;
+    [SerializeField] float angleCorrection = 90;
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - angleCorrection;  // Angle correction as the sprites rotates 90 degrees extra
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
