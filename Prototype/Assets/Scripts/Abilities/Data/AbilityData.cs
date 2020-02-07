f042da2e6cb265389c1ce6419e6af738dc62a8f7
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-07 16:48:56 +0100

    1 ability works

---
+++ Prototype/Assets/Scripts/Abilities/Data/AbilityData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AbilityData : MonoBehaviour
+{
+    public AbilityDescription description;
+    public AbilityStats stats;
+}
+
+[System.Serializable]
+public class AbilityStats
+{
+    public int cooldown;      // Time till you can cast the ability again
+    public int manaCost;      // How much mana will this ability cost?
+    public int dotDamage;     // Damage over time damage tick
+    public int dotDuration;   // Damage over time durration
+    public int hpDiff;        // If positive it's a heal if it's negative it's damage 
+}
+
+[System.Serializable]
+public class AbilityDescription
+{ 
+    public string name;
+    public string casterTeamName;
+    //public bool isTrap;
+}
---
+++ Prototype/Assets/Scripts/Abilities/MoveAbility.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAbility : MonoBehaviour
+{
+    // The postion towards which the ability will go to
+    Vector3 moveDirection;
+
+    // Speed at which the projectile will move to the target
+    public float speed;
+
+    void OnEnable()
+    {
+        Debug.Log("MoveAbility::OnEnable");
+        moveDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        moveDirection.z = 0;
+        moveDirection.Normalize();
+    }
+
+    float time = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + moveDirection * speed * Time.deltaTime;
+
+        if (Time.realtimeSinceStartup > time)
+        {
+            gameObject.SetActive(false);
+            time *= 2;
+        }
+            
+    }
+}
