293112d59ffd28f6b1ba67496ee7bde99887b86c
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-15 14:09:40 +0100

    Removed extra code from ProjectileAbility

--- Prototype/Assets/Scripts/Abilities/ProjectileAbility.cs
+++ Prototype/Assets/Scripts/Abilities/ProjectileAbility.cs
@@ -10,9 +10,6 @@ public class ProjectileAbility : Ability
 
     [SerializeField] Transform castOrigin;
 
-    // If true we will rotate the projectile to face the direction in which it is shot
-    [SerializeField] bool rotateToCastDirection;
-
     public override void Load()
     {
         base.Load();
@@ -20,19 +17,12 @@ public class ProjectileAbility : Ability
         Debug.Log("ProjectileAbility chceking stats name is " + abilityData.description.name);
     }
 
-    Quaternion rotation;
-
     public override void Cast()
     {
         base.Cast();
 
         Debug.Log("ProjectileAbility is casting " + abilityData.description.name);
-
-        if(rotateToCastDirection)
-        {
-            rotation = 
-        }
-
+                
         Instantiate(projectile, castOrigin.position, Quaternion.identity);
 
         projectile.transform.position = transform.position;
