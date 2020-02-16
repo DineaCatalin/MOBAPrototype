55c5ae8847a51d26851ab34b426668d76f71bac2
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-16 19:50:06 +0100

    Added Rush Ability and rush area to player

---
+++ Prototype/Assets/Scripts/Abilities/Effects/RushArea.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This will not inherit from AbilityEffect as we don't want to implement ApllyEffect
+// We will add a speed value on enter and remove it on exit of the collider
+public class RushArea : MonoBehaviour
+{
+    [SerializeField] new string name;
+
+    AbilityData abilityData;
+    float speedBoost;
+
+    private void Start()
+    {
+        abilityData = AbilityDataCache.GetDataForAbility(name);
+        speedBoost = abilityData.stats.dotValue;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == abilityData.description.casterTeamName)
+        {
+            Player player = collision.GetComponent<Player>();
+            player.GetStats().speed += speedBoost;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == abilityData.description.casterTeamName)
+        {
+            Player player = collision.GetComponent<Player>();
+            player.GetStats().speed -= speedBoost;
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/Abilities/RushAbility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RushAbility : Ability
+{
+    [SerializeField] GameObject rushArea;
+
+    public override void Cast()
+    {
+        base.Cast();
+
+        rushArea.SetActive(true);
+
+        StartCoroutine(RemoveRushArea(abilityData.stats.duration));
+    }
+
+    IEnumerator RemoveRushArea(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+        rushArea.SetActive(false);
+    }
+}
