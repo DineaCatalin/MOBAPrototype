3ae3e247f2cafe9ee6676171b5ab93ead320a432
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-16 17:19:40 +0100

    Earthquake and dummy controller for enemy player

--- Prototype/Assets/Scripts/Player/PlayerDataLoader.cs
+++ Prototype/Assets/Scripts/Player/PlayerDataLoader.cs
@@ -3,16 +3,13 @@ using System.Collections;
 
 public static class PlayerDataLoader
 {
-    static PlayerData data;
-
     public static PlayerData Load()
     {
-        if(data == null)
-        {
-            string dataString = FileHandler.ReadString("PlayerConfig");
-//            Debug.Log(dataString);
-            data = JsonUtility.FromJson<PlayerData>(dataString);
-        }
+        PlayerData data = new PlayerData();
+
+        string dataString = FileHandler.ReadString("PlayerConfig");
+        //            Debug.Log(dataString);
+        data = JsonUtility.FromJson<PlayerData>(dataString);
 
         return data;
     }
---
+++ Prototype/Assets/Scripts/Test/DummyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyController : MonoBehaviour
+{
+    Vector3 movementIncrement;
+
+    [SerializeField] float speed = 5f;
+
+    private void Update()
+    {
+        movementIncrement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))       // UP
+        {
+            movementIncrement += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))       // DOWN
+        {
+            movementIncrement += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))       // LEFT
+        {
+            movementIncrement += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))       // RIGHT
+        {
+            movementIncrement += Vector3.right;
+        }
+
+        transform.Translate(movementIncrement * Time.deltaTime * speed, Space.World);
+    }
+}
