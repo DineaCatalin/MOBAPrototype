9cb9f9371ae64e361c0ded034352207300af0010
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-18 16:08:36 +0100

    Added PlayerSync for transform and Rigidbody and Roomcontroller script

---
+++ Prototype/Assets/Scripts/Network/PUN2_PlayerSync.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class PUN2_PlayerSync : MonoBehaviourPun, IPunObservable
+{
+    //List of the scripts that should only be active for the local player (ex. PlayerController, MouseLook etc.)
+    public MonoBehaviour[] localScripts;
+
+    //List of the GameObjects that should only be active for the local player (ex. Camera, AudioListener etc.)
+    public GameObject[] localObjects;
+
+    //Values that will be synced over network
+    Rigidbody2D rb;
+
+    Vector3 latestPos;
+    Quaternion latestRot;
+    Vector3 velocity;
+    float angularVelocity;
+
+    bool valuesReceived;
+
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            // Player is local
+            rb = GetComponent<Rigidbody2D>();
+
+            // Set main healthbar to this player
+        }
+        else
+        {
+            //Player is Remote, deactivate the scripts and object that should only be enabled for the local player
+            for (int i = 0; i < localScripts.Length; i++)
+            {
+                localScripts[i].enabled = false;
+            }
+            for (int i = 0; i < localObjects.Length; i++)
+            {
+                localObjects[i].SetActive(false);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!photonView.IsMine && valuesReceived)
+        {
+            //Update Object position and Rigidbody parameters
+            transform.position = Vector3.Lerp(transform.position, latestPos, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, latestRot, Time.deltaTime * 5);
+            rb.velocity = velocity;
+            rb.angularVelocity = angularVelocity;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(rb.velocity);
+            stream.SendNext(rb.angularVelocity);
+        }
+        else
+        {
+            //Network player, receive data
+            latestPos = (Vector3)stream.ReceiveNext();
+            latestRot = (Quaternion)stream.ReceiveNext();
+            velocity = (Vector3)stream.ReceiveNext();
+            angularVelocity = (float)stream.ReceiveNext();
+
+            valuesReceived = true;
+        }
+    }
+}
