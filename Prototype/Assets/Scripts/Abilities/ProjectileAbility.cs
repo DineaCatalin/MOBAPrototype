bd45093e7143caaae48df38993aa4a356831392d
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-02-28 11:42:55 +0100

    Fixed Blast and Pull not spawning correctly after network sync

--- Prototype/Assets/Scripts/Abilities/ProjectileAbility.cs
+++ Prototype/Assets/Scripts/Abilities/ProjectileAbility.cs
@@ -8,39 +8,66 @@ public class ProjectileAbility : Ability
     // The projectile it self will know if it moves or what it does on collision
     [SerializeField] GameObject projectile;
 
-    [SerializeField] Transform castOrigin;
+    // Transform of the location where we will spawn
+    Transform castOrigin;
+
+    // Position where we will spawn, we use this varibale because we want to change the z value
+    // without affecting the players z value
+    Vector3 castPosition;
+
+    [SerializeField] bool mimicPlayerRotation = false;
+
+    [Tooltip("If true will use the cast origin that is a child of the player, otherwise it will use the players position")]
+    [SerializeField] bool useCastOrigin = true;
 
     int projectileLayer;
     string projectileName;
 
+    // Used when we want to spawn a projectile facing the mouse direction
+    //Vector3 spawnDirection;
+    //float spawnAngle;
+    Quaternion spawnRotation;
+
     public override void Load()
     {
         base.Load();
-
-//        Debug.Log("ProjectileAbility chceking stats name is " + abilityData.description.name);
     }
 
     private void Start()
     {
-        if (castOrigin == null)
+        if (useCastOrigin)
         {
             castOrigin = GameObject.Find("CastOrigin" + playerID).transform;
-
-            // Get layer name for the projectile
-            // If its the player is in team1 the layer name will be Team1Ability if player is in team2 the layer name will be Team2Ability
-            string layerName = LayerMask.LayerToName(GameObject.Find("Player" + playerID).layer);
-            layerName = layerName.Replace("Player", "Ability");
-
-            projectileLayer = LayerMask.NameToLayer(layerName);
-            projectileName = projectile.name.Replace("(Clone)", "");
         }
+        else
+        {
+            castOrigin = GameObject.Find("Player" + playerID).transform;
+        }
+
+        // Get layer name for the projectile
+        // If its the player is in team1 the layer name will be Team1Ability if player is in team2 the layer name will be Team2Ability
+        string layerName = LayerMask.LayerToName(GameObject.Find("Player" + playerID).layer);
+        layerName = layerName.Replace("Player", "Ability");
+
+        projectileLayer = LayerMask.NameToLayer(layerName);
+        projectileName = projectile.name.Replace("(Clone)", "");
     }
 
     public override void Cast()
     {
         base.Cast();
 
-        AbilitySpawner.Instance.SpawnProjectile(projectileName, castOrigin.position, Quaternion.identity, Camera.main.ScreenToWorldPoint(Input.mousePosition), projectileLayer);
+        if (mimicPlayerRotation)
+            spawnRotation = spellIndicator.transform.rotation;
+        else
+            spawnRotation = Quaternion.identity;
+
+        if (useCastOrigin)
+            castPosition = castOrigin.position;
+        else           // We are spawning the projectile in the middle of the player so make it appear under the player
+            castPosition = new Vector3(castOrigin.position.x, castOrigin.position.y, 1);
+
+        AbilitySpawner.Instance.SpawnProjectile(projectileName, castPosition, spawnRotation, Camera.main.ScreenToWorldPoint(Input.mousePosition), projectileLayer);
     }
 
 }
