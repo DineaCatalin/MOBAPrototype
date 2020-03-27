0efcefba3b9b8c3200f320ecf8f31bc29c35d2b6
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-03-27 15:56:31 +0100

    Added pillars and new sprites

---
+++ Prototype/Assets/Scripts/Utils/FullScreenSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenSprite : MonoBehaviour
+{
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+       // transform = GetComponent<Transform>();
+
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 lScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+
+        transform.localScale = lScale;
+    }
+}
