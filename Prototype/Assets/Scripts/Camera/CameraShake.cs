595f9cb5c4a21e0d5035be3e08025b4a6ceb2b00
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-03-28 14:07:37 +0100

    Spell indicator code is commented because all abilities are instant for the moment so we need no spell indicators, changed secondary ability keys to x c v b

---
+++ Prototype/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class encapsulates all needed behaviour for a camera shake
+public class CameraShake : MonoBehaviour {
+
+    public Camera mainCamera;
+
+    private Vector3 cameraOrigin;
+    private float shakeAmount = 0;
+
+    private void Awake()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+
+        // Cahce the original position of the camera so that we know where to
+        cameraOrigin = mainCamera.transform.position;
+    }
+
+    public void Shake(float amount, float length)
+    {
+        shakeAmount = amount;
+        InvokeRepeating("Shake", 0, 0.01f);
+        Invoke("StopShake", length);
+    }
+
+    void Shake()
+    {
+        if(shakeAmount > 0)
+        {
+            Vector3 camPos = mainCamera.transform.position;
+
+            float OFFSET_X = Random.value * shakeAmount * 2 - shakeAmount;
+            float OFFSET_Y = Random.value * shakeAmount * 2 - shakeAmount;
+
+            camPos.x += OFFSET_X;
+            camPos.y += OFFSET_Y;
+
+            mainCamera.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("Shake");
+        mainCamera.transform.position = cameraOrigin;
+    }
+}
