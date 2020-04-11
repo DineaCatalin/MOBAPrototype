dc2ba436753ca58678ef0db7c0b4298bd03a4aed
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-04-11 17:40:22 +0200

    Added Fade to Dust Dusk

---
+++ Prototype/Assets/Scripts/UI/Tweens/BulkScaleTween.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulkScaleTween : MonoBehaviour
+{
+    public Vector2 initialScale;
+    public Vector2 finalScale;
+    public float scaleTime;
+    public LeanTweenType easeType;
+
+    public GameObject[] tweenedObjects;
+
+    private void Awake()
+    {
+        SetInitialScale();
+    }
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        foreach (GameObject tweenedObj in tweenedObjects)
+        {
+            LeanTween.scale(tweenedObj, finalScale, scaleTime).setEase(easeType);
+        }
+    }
+
+    private void OnDisable()
+    {
+        SetInitialScale();
+    }
+
+    void SetInitialScale()
+    {
+        foreach (GameObject tweenedObj in tweenedObjects)
+        {
+            tweenedObj.transform.localScale = initialScale;
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/UI/Tweens/FadeTween.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTween : MonoBehaviour
+{
+    public float initialAlpha;
+    public float finalAlpha;
+    public float duration;
+    public LeanTweenType easeType;
+
+    SpriteRenderer spriteRenderer;
+
+    Color color;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        color = spriteRenderer.color;
+    }
+
+    private void OnEnable()
+    {
+        setSpriteAlpha(initialAlpha);
+        LeanTween.value(gameObject, setSpriteAlpha, initialAlpha, finalAlpha, duration).setEase(easeType);
+    }
+
+    void setSpriteAlpha(float val)
+    {
+        color.a = val;
+        spriteRenderer.color = color;
+    }
+}
