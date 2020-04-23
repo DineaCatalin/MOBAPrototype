4378d0e717ab35ecf448b78877e545aa4f67fa58
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-04-24 01:12:17 +0200

    Changed visuales for Root

---
+++ Prototype/Assets/Scripts/Collider/ColliderDelayedActivation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDelayedActivation : MonoBehaviour
+{
+    public float activationTime; 
+
+    Collider2D objectCollider;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        objectCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Invoke("EnableCollider", activationTime);
+    }
+
+    void EnableCollider()
+    {
+        objectCollider.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        objectCollider.enabled = false;
+    }
+}
