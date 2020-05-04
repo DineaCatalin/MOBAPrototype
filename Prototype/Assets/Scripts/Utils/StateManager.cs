11d7222c6a30cff094f9f732e1e9ade809ddcf18
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-05-04 12:31:02 +0200

    Changed Rush Area VFX logic

--- Prototype/Assets/Scripts/Utils/StateManager.cs
+++ Prototype/Assets/Scripts/Utils/StateManager.cs
@@ -5,13 +5,27 @@ using System.Collections;
 public class StateManager : MonoBehaviour
 {
     [SerializeField] GameObject managedObject;
+    [SerializeField] Collider2D objectCollider;
+    [SerializeField] LocalParticleSystemManager particles;
+    [SerializeField] AbilityScaleTween tween;
 
     Coroutine deactivate;
+
+    private void Awake()
+    {
+        objectCollider = GetComponentInChildren<Collider2D>();
+
+        particles = GetComponentInChildren<LocalParticleSystemManager>();
+        particles.enabled = false;
+
+        tween = GetComponentInChildren<AbilityScaleTween>();
+        tween.enabled = false;
+    }
 
     public void Activate(float duration)
     {
         Debug.Log("StateManager Activate " + managedObject.name);
-        managedObject.SetActive(true);
+        Activate();
 
         if (deactivate != null)
             StopCoroutine(deactivate);
@@ -25,7 +39,7 @@ public class StateManager : MonoBehaviour
 
         Debug.Log("StateManager Activate " + managedObject.name);
 
-        managedObject.SetActive(false);
+        Deactivate();
     }
 
     public void SetLayer(int layer)
@@ -35,6 +49,17 @@ public class StateManager : MonoBehaviour
 
     public void Deactivate()
     {
-        managedObject.SetActive(false);
+        //managedObject.SetActive(false);
+        objectCollider.enabled = false;
+        particles.DeactivateParticleSystems();
+        tween.enabled = false;
     }
+
+    void Activate()
+    {
+        objectCollider.enabled = true;
+        tween.enabled = true;
+        particles.ActivateParticleSystems();
+    }
+
 }
