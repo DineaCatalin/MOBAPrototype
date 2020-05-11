8ffe40980411d619ff4e62c9c6d055bd8cf5bada
Dinea Catalin <catalin.dinea@aeriagames.com>
2020-05-12 00:52:03 +0200

    Player spawn with synchs with lag, still not complete

---
+++ Prototype/Assets/Scripts/Network/NetworkUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class NetworkUtils : MonoBehaviourPun, IPunObservable
+{
+    public const float PLAYER_SPAWN_DELAY = 1f;
+    public float lag;
+
+    public static NetworkUtils SharedInstance;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+        Debug.Log("NetworkUtils lag : " + lag);
+    }
+
+    public float NonLocalPlayerSpawnDelay()
+    {
+        Debug.LogError("NetworkUtils NonLocalPlayerSpawnDelay " + (PLAYER_SPAWN_DELAY - lag) + " lag = " + lag);
+        return PLAYER_SPAWN_DELAY - lag;
+    }
+}
